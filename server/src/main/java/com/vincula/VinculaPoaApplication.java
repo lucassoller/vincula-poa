8c851bcc4396b4d8bed8d6355c458164be814daa
lucassoller <lucassoller2000@gmail.com>
2026-04-16 02:12:39 -0300

    criando estrutura do projeto

---
+++ server/src/main/java/com/vincula/VinculaPoaApplication.java
@@ -0,0 +1,13 @@
+package com.vincula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinculaPoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinculaPoaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/vincula/VinculaPoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinculaPoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
